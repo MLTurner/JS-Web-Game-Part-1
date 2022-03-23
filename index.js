0a6e51f943b13742429d17a830fad789b8bcae82
MLTurner <mlturne3@ncsu.edu>
2022-03-23 19:26:05 -0400

    added more characters

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function newImage (){
-    let greenCharacter = document.createElement('img')
-    greenCharacter.src = 'assets/green-character.gif'
-    greenCharacter.style.position = 'fixed'
-    greenCharacter.style.left = '100px'
-    greenCharacter.style.bottom = '100px'
-    document.body.append(greenCharacter)  
+function newImage (url, left, bottom){
+    let object = url,
+    
+    
+    
+    document.body.append ()
+    console.log()
 }
+
+newImage('assets/green-character.gif',left, bottom)
+
+
 
 
 let greenCharacter = document.createElement('img')
